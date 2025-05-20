b8ecf7447f938db6da175e81138604b48817368c
Rajatprogrammerr <rpurpose81@gmail.com>
2025-05-20 19:33:10 +0530

    Form issue resolved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    '/contact': 'http://localhost:5000',
+  },
 })
